a7e2560270628e9e95b4cccc3188a08d4653eeaa
Expert Snippet <g12360337@gmail.com>
2025-08-02 01:14:53 +0530

    secondcommit

--- config/inngest.js
+++ config/inngest.js
@@ -8,11 +8,11 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 //Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
     {
-        id: 'sync-user-from-clerk',
+        id: 'create-user-from-clerk'
     },
-    { event: 'clerk/user.created' },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    {event: 'clerk/user.created' },
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url } = event.data;
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
