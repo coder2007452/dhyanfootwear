f08bd9db9703658139d6ee86c6dcd4adc3dfb6be
Expert Snippet <g12360337@gmail.com>
2025-08-02 14:43:26 +0530

    everything new

--- config/inngest.js
+++ config/inngest.js
@@ -5,16 +5,16 @@ import User from "@/models/User";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 
-//Inngest Function to save user data to a database
+//Inngest Function to create user data to a database
 export const syncUserCreation = inngest.createFunction(
     {
         id: 'sync-user-from-clerk'
     },
-    { event: 'clerk/user.created' },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data
+    {event: 'clerk/user.created'},
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
-            _id: id,
+            _id:id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
@@ -29,11 +29,11 @@ export const syncUserUpdation = inngest.createFunction(
     {
         id: 'update-user-from-clerk'
     },
-    { event: 'clerk/user.updated' },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data
+    {event: 'clerk/user.updated'},
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
-            _id: id,
+            _id:id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             imageUrl: image_url
@@ -44,13 +44,13 @@ export const syncUserUpdation = inngest.createFunction(
 )
 
 //Inngest Function to delete user data to a database
-export const syncUserDeletion = inngest.createFunction(
+export const syncUserDelection = inngest.createFunction(
     {
         id: 'delete-user-with-clerk'
     },
-    { event: 'clerk/user.deleted' },
-    async ({ event }) => {
-        const { id } = event.data
+    {event: 'clerk/user.deleted'},
+    async ({event}) => {
+        const {id} = event.data
 
         await connectDB()
         await User.findByIdAndDelete(id)
--- models/User.js
+++ models/User.js
@@ -5,9 +5,9 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     imageUrl: {type: String, required: true},
-    cartItems: {type: Object, required: true},
+    cartItems: {type: Object, default: {}}
 }, {minimize: false});
 
 const User = mongoose.models.user || mongoose.model("user", userSchema)
 
-export default User;
+export default User
