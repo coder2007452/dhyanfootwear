d98e658655b3f74110f4cca20f0e0bc38f9faca8
Expert Snippet <g12360337@gmail.com>
2025-08-02 14:22:22 +0530

    some

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+let cached = global.mongoose
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+    cached = global.mongoose = { conn: null, promise: null }
 }
 
 async function connectDB() {
     if (cached.conn) {
-        return cached.conn;
+        return cached.conn
     }
 
     if (!cached.promise) {
